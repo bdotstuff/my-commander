67fa411f4753ae7e1854740263ace3c3382f05d3
__b <serban_tudoralexandru@yahoo.com>
2025-12-09 16:10:00 +0200

    double click + icons

--- main.cpp
+++ main.cpp
@@ -11,12 +11,13 @@
 namespace fs = std::filesystem;
 
 // Definim aici constante, simplifica foarte mult modificarea acestora in mai multe parti ale codului, si sunt mai usor de gasit
-#define POS_OFFSET         25
+#define POS_OFFSET          25
 //#define BOX_WIDTH         150 // asta e ajustabila in functie de alte tabele stanga/dreapta + resize, ar trebui calculata manual
-#define BOX_HEIGHT         20
-#define PADDING_LEFT        5
-#define PADDING_TOP        50
-#define LINE_PADDING       10
+#define BOX_HEIGHT          20
+#define PADDING_LEFT        10
+#define PADDING_TOP         90
+#define PADDING_TOP_HISTORY 40
+#define LINE_PADDING        0
 
 // Mult mai ok decat un string, mai eficient in spatiu de memorie si in comparare (un simplu switch)
 enum sortingType{
@@ -25,6 +26,10 @@ enum sortingType{
     SORT_TYPE,
     SORT_SIZE,
 };
+
+std::chrono::steady_clock::time_point currentClickTime;
+std::chrono::steady_clock::time_point lastClickTime;
+const int doubleClickThreshold = 500;
 
 struct entry {
     fs::path path;
@@ -44,17 +49,63 @@ struct fileWindow {
     float scrollAmount; // Offset-ul pozitiei butoanelor atunci cand dai scroll
 } filewindow[2], historywindow[2];
 
-sf::Font font_listFile("utfont.ttf");
-
 // Este mai bine path in loc de index. Asa putem folosi aceeasi functie si pentru filewindow, cat si pentru historywindow
 // De obs totusi ca conteaza ordinea in care se apeleaza functia
-fs::path _get_hover_path(fileWindow filewindow, sf::RenderWindow &window, int side) {
+fs::path _get_hover_path(fileWindow filewindow, sf::RenderWindow &window) {
     for (int i = 0; i < filewindow.list.size(); i++) {
         if (filewindow.list[i].areaBox.contains(sf::Vector2<float>(sf::Mouse::getPosition(window))))
             return filewindow.list[i].path;
     }
     return filewindow.currentPath;
 }
+
+int _get_hover_path_index(fileWindow filewindow, sf::RenderWindow &window, int side) {
+    for (int i = 0; i < filewindow.list.size(); i++) {
+        if (filewindow.list[i].areaBox.contains(sf::Vector2<float>(sf::Mouse::getPosition(window))))
+            return i;
+    }
+    return -1;
+}
+
+void _draw_ui(sf::RenderWindow &window) {
+    //drawing background bar
+    sf::RectangleShape bgbar;
+    bgbar.setPosition(-sf::Vector2f(0, 420)); // border top
+    bgbar.setFillColor(sf::Color::White);
+    bgbar.setSize(sf::Vector2f(window.getSize().x, 500));
+    window.draw(bgbar);
+
+    bgbar.setPosition(sf::Vector2f(0, 50)); // history bar bg
+    bgbar.setFillColor(sf::Color(58, 52, 74));
+    bgbar.setSize(sf::Vector2f(window.getSize().x, 25));
+    window.draw(bgbar);
+
+    bgbar.setPosition(sf::Vector2f(0, 0)); // border left
+    bgbar.setFillColor(sf::Color::White);
+    bgbar.setSize(sf::Vector2f(2, window.getSize().y));
+    window.draw(bgbar);
+
+    bgbar.setPosition(sf::Vector2f(window.getSize().x - 2, 0)); // border right
+    bgbar.setFillColor(sf::Color::White);
+    bgbar.setSize(sf::Vector2f(2, window.getSize().y));
+    window.draw(bgbar);
+
+    bgbar.setSize(sf::Vector2f(5, window.getSize().y)); // middle split
+    bgbar.setPosition(sf::Vector2f(window.getSize().x / 2 - 2.5, 0));
+    bgbar.setFillColor(sf::Color::White);
+    window.draw(bgbar);
+
+    bgbar.setPosition(sf::Vector2f(0, 20)); // top split (icons/shortcuts, whatever was there)
+    bgbar.setFillColor(sf::Color(58, 52, 74));
+    bgbar.setSize(sf::Vector2f(window.getSize().x, 1));
+    window.draw(bgbar);
+}
+
+sf::Font font_listFile("utfont.ttf");
+fs::path cmdIconPlaceholder = "cmdIconPlaceholder.png";
+fs::path dirIcon = "dirIcon.png";
+fs::path regularIcon = "regularFile.png";
+fs::path backIcon = "backIcon.png";
 
 // Doar actualizam partial fiecare filewindow, nu e nevoie de variabile temporare, etc.
 void _draw_lists(fileWindow filewindow[2], sf::RenderWindow &window){
@@ -64,7 +115,19 @@ void _draw_lists(fileWindow filewindow[2], sf::RenderWindow &window){
     for(int side=0;side<2;side++){
         for(int i=0;i<filewindow[side].list.size();i++){
             pos = {(float)PADDING_LEFT+side*((window.getSize().x)/2+LINE_PADDING), (float)i*POS_OFFSET+filewindow[side].scrollAmount+PADDING_TOP};
-            size = {(float)(window.getSize().x)/2 - 20, BOX_HEIGHT};
+            size = {(float)(window.getSize().x)/2 - 40, BOX_HEIGHT};
+
+            sf::Texture texture;
+            if (filewindow[side].list[i].path == filewindow[side].currentPath.parent_path()) texture.loadFromFile(backIcon);
+            else if (is_directory(filewindow[side].list[i].path)) texture.loadFromFile(dirIcon);
+            else texture.loadFromFile(regularIcon);
+            sf::Sprite iconSprite(texture);
+            iconSprite.setPosition(pos + sf::Vector2f(0, 3));
+            window.draw(iconSprite);
+
+            pos += sf::Vector2f(20, 0);
+            size -= sf::Vector2f(20, 0);
+
             sf::RectangleShape box;
             box.setPosition(pos);
             box.setSize(size);
@@ -79,11 +142,12 @@ void _draw_lists(fileWindow filewindow[2], sf::RenderWindow &window){
                 else box.setOutlineColor(sf::Color(0, 0, 0, 0));
             }
             text.setPosition(pos);
-            if (areaBox.contains(sf::Vector2<float>(sf::Mouse::getPosition(window)))) text.setPosition(pos + sf::Vector2<float>(5, 0));
+            if (areaBox.contains(sf::Vector2<float>(sf::Mouse::getPosition(window))) or filewindow[side].list[i].selected == true)
+                text.setPosition(pos + sf::Vector2<float>(5, 0));
             text.setFont(font_listFile);
             text.setCharacterSize(16);
             text.setFillColor(sf::Color::White);
-            text.setString(filewindow[side].list[i].path.filename().string());
+            text.setString(filewindow[side].list[i].name);
 
             // Nu se deseneaza peste historywindow
             if(pos.y >= PADDING_TOP){
@@ -98,23 +162,6 @@ void _draw_lists(fileWindow filewindow[2], sf::RenderWindow &window){
 }
 
 fileWindow _draw_list_history_button(sf::Vector2f pos, fileWindow file, sf::RenderWindow &window) {
-    //drawing background bar
-    sf::RectangleShape bgbar;
-    bgbar.setPosition(pos - sf::Vector2f(0, 500));
-    bgbar.setFillColor(sf::Color::White);
-    bgbar.setSize(sf::Vector2f(window.getSize().x, 500));
-    window.draw(bgbar);
-
-    bgbar.setPosition(pos);
-    bgbar.setFillColor(sf::Color(58, 52, 74));
-    bgbar.setSize(sf::Vector2f(window.getSize().x, 25));
-    window.draw(bgbar);
-
-    bgbar.setPosition(pos + sf::Vector2f(0, 25));
-    bgbar.setFillColor(sf::Color::White);
-    bgbar.setSize(sf::Vector2f(window.getSize().x, 5));
-    window.draw(bgbar);
-
     entry button;
     float additional_x_position;
     fileWindow history;
@@ -135,7 +182,7 @@ fileWindow _draw_list_history_button(sf::Vector2f pos, fileWindow file, sf::Rend
     history.list.push_back(temp);
 
     while (history.list.size() > 0) { // calculating how many paths can be displayed (for auto-resize)
-        additional_x_position = 0;
+        additional_x_position = PADDING_LEFT;
         for (int j=0; j < history.list.size(); j++) {
             sf::Text text(font_listFile);
             text.setString(history.list[j].path.filename().string());
@@ -150,18 +197,18 @@ fileWindow _draw_list_history_button(sf::Vector2f pos, fileWindow file, sf::Rend
         else break;
     }
 
-    additional_x_position = 0;
+    additional_x_position = PADDING_LEFT;
     for (int i = history.list.size() - 1; i >= 0; i--) { // drawing the directories in their actual order (history contains them last to first)
         sf::Text text(font_listFile);
         text.setString(history.list[i].name);
         text.setCharacterSize(16);
-        text.setPosition(pos + sf::Vector2f(additional_x_position, 0));
+        text.setPosition(pos + sf::Vector2f(additional_x_position, PADDING_TOP_HISTORY));
 
-        sf::Rect<float> areaBox(pos + sf::Vector2<float>(additional_x_position, 4), text.getGlobalBounds().size);
+        sf::Rect<float> areaBox(pos + sf::Vector2<float>(additional_x_position, PADDING_TOP_HISTORY + 4), text.getGlobalBounds().size);
         history.list[i].areaBox = areaBox;
 
         sf::RectangleShape box;
-        box.setPosition(pos + sf::Vector2<float>(additional_x_position, 20));
+        box.setPosition(pos + sf::Vector2<float>(additional_x_position, PADDING_TOP_HISTORY + 20));
         box.setSize(sf::Vector2f(text.getGlobalBounds().size.x, 0));
         box.setFillColor(sf::Color(0, 0, 0, 0));
         box.setOutlineThickness(1);
@@ -183,7 +230,7 @@ fileWindow _draw_list_history_button(sf::Vector2f pos, fileWindow file, sf::Rend
 
         if (i > 0) {
             text.setString(">");
-            text.setPosition(pos + sf::Vector2f(additional_x_position, 0));
+            text.setPosition(pos + sf::Vector2f(additional_x_position, PADDING_TOP_HISTORY));
             text.setCharacterSize(16);
             window.draw(text);
             additional_x_position += 2 + text.getGlobalBounds().size.x;
@@ -209,12 +256,19 @@ void _update_window(fileWindow &window, fs::path newPath) {
     window.list.clear();
     window.currentPath = newPath;
 
-    for (auto e : fs::directory_iterator(newPath))
+    if (newPath != newPath.root_path())
+        window.list.push_back({.path = newPath.parent_path(), .selected = false, .name = "[..]"});
+
+    fs::path texPath;
+    for (auto e : fs::directory_iterator(newPath)) {
         if(!(GetFileAttributes(e.path().string().c_str()) & FILE_ATTRIBUTE_HIDDEN))
             window.list.push_back({.path = e.path(), .selected = false, .name = e.path().filename().string()});
+    }
+
 
     // Sortare:
     auto first = window.list.begin();
+    if (newPath != newPath.root_path()) first = window.list.begin() + 1;
     auto last = window.list.end();
     switch(window.sort){
         case SORT_NAME:{
@@ -336,18 +390,13 @@ int main()
     {
         window.clear(sf::Color(20, 23, 36));
 
+        _draw_ui(window);
+
         historywindow[0] = _draw_list_history_button(sf::Vector2f(0, 10), filewindow[0], window);
-        historywindow[1] = _draw_list_history_button(sf::Vector2f(window.getSize().x / 2 + 7, 10), filewindow[1], window);
+        historywindow[1] = _draw_list_history_button(sf::Vector2f(window.getSize().x / 2, 10), filewindow[1], window);
 
         if (sf::Mouse::getPosition(window).x > window.getSize().x / 2 + LINE_PADDING) current = 1;
         else current = 0;
-
-        sf::RectangleShape line;
-        line.setSize(sf::Vector2f(5, window.getSize().y));
-        line.setPosition(sf::Vector2f(window.getSize().x / 2 - 2.5, 0));
-        line.setFillColor(sf::Color::White);
-
-        window.draw(line);
 
         _draw_lists(filewindow, window);
 
@@ -363,8 +412,24 @@ int main()
             if (event->is<sf::Event::MouseButtonPressed>()) {
                 if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
                     _print_list(filewindow[0].list);
-                    _command_open(filewindow[current], _get_hover_path(historywindow[current], window, current));
-                    _command_open(filewindow[current], _get_hover_path(filewindow[current], window, current));
+
+                    _command_open(filewindow[current], _get_hover_path(historywindow[current], window)); // do this first, otherwise there's conflicts on double click
+
+                    currentClickTime = std::chrono::steady_clock::now();
+                    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(currentClickTime - lastClickTime).count();
+                    lastClickTime = std::chrono::steady_clock::now();
+                    if (duration < doubleClickThreshold) // Open ONLY if double clicked
+                    {
+                        std::cout << "dblclck" << std::endl;
+                        _print_list(filewindow[current].list);
+                        if (filewindow[current].list[_get_hover_path_index(filewindow[current], window, current)].selected == true)
+                            _command_open(filewindow[current], _get_hover_path(filewindow[current], window));
+                    }
+
+                    for (int i = 0; i < filewindow[current].list.size(); i++) _command_deselect(filewindow[current], i);
+                    for (int i = 0; i < filewindow[1-current].list.size(); i++) _command_deselect(filewindow[1-current], i); // clears all selection
+
+                    _command_select(filewindow[current], _get_hover_path_index(filewindow[current], window, current)); // selecting
                 }
             }
             if(event->is<sf::Event::KeyPressed>()){
